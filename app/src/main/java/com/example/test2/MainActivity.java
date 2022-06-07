7c23cb9587a9f1d2e80a34e4f20ac3e65503304a
Kostya3521 <kostya.fommin.02@bk.ru>
2022-06-07 16:21:03 +0500

    version2

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -10,26 +10,23 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    private SharedPreferences pref;
-    private String key_count = "count";
-    private int count;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        init();
-    }
-
-    private void init(){
-        pref = getSharedPreferences("Share", MODE_PRIVATE);
+        SharedPreferences pref = getSharedPreferences("Share", MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
-        count = pref.getInt(key_count, 0);
+        String key_count = "count";
+        int count = pref.getInt(key_count, 0);
         editor.putInt(key_count, ++count);
         editor.apply();
         if (count % 3 == 0){
             Toast.makeText(MainActivity.this, "Вот это да", Toast.LENGTH_LONG).show();
         }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
     }
+
+
 }
